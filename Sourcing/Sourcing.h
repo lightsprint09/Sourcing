e92918ad31b86b0212999d27feef80d71b196523
Lukas Schmidt <lukas.schmi@gmail.com>
2016-05-25 11:47:51 +0200

    Initial Commit

---
+++ Sourcing/Sourcing.h
@@ -0,0 +1,19 @@
+//
+//  Sourcing.h
+//  Sourcing
+//
+//  Created by Lukas Schmidt on 25.05.16.
+//  Copyright © 2016 Lukas Schmidt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sourcing.
+FOUNDATION_EXPORT double SourcingVersionNumber;
+
+//! Project version string for Sourcing.
+FOUNDATION_EXPORT const unsigned char SourcingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sourcing/PublicHeader.h>
+
+
