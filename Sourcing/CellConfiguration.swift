134f389c07ac2eaabb36e832edb028c4ff3f9954
Lukas Schmidt <lukas.schmi@gmail.com>
2016-05-27 20:47:16 +0200

    Importe public interface

--- Sourcing/CellConfiguration.swift
+++ Sourcing/CellConfiguration.swift
@@ -9,29 +9,22 @@
 import Foundation
 import UIKit
 
-struct CellConfiguration<Object, Cell: UICollectionViewCell where Cell: ConfigurableCell, Cell.DataSource == Object>: CellDequeable {
-    let cellIdentifier: String
-    let nib: UINib?
+public struct CellConfiguration<Object, Cell: UICollectionViewCell where Cell: ConfigurableCell, Cell.DataSource == Object>: CellDequeable {
+    public let cellIdentifier: String
+    public let nib: UINib?
     let additionalConfiguartion: ((Object, Cell) -> Void)?
     
-    init(cellIdentifier: String, nib: UINib? = nil, additionalConfiguartion: ((Object, Cell) -> Void)? = nil) {
+    public  init(cellIdentifier: String, nib: UINib? = nil, additionalConfiguartion: ((Object, Cell) -> Void)? = nil) {
         self.cellIdentifier = cellIdentifier
         self.nib = nib
         self.additionalConfiguartion = additionalConfiguartion
     }
     
-    func dequeueReusableCell(collectionView: UICollectionView, indexPath: NSIndexPath, object: Any) ->  UICollectionViewCell? {
-        if object is Object {
-            return collectionView.dequeueReusableCellWithReuseIdentifier(cellIdentifier, forIndexPath: indexPath)
-        }
-        return nil
-    }
-    
-    func canConfigurecellForItem(object: Any) -> Bool {
+    public  func canConfigurecellForItem(object: Any) -> Bool {
         return object is Object
     }
     
-    func configureCell(cell: AnyObject, object: Any) -> AnyObject {
+    public func configureCell(cell: AnyObject, object: Any) -> AnyObject {
         if let object = object as? Object, cell = cell as? Cell {
             cell.configureForObject(object)
             additionalConfiguartion?(object, cell)
