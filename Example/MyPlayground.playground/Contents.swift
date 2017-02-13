2043a33dd8bf4e11a4a82e3a366e418dbaa15dd9
Terenze Yuen <terenzeyuen@gmail.com>
2017-02-13 15:15:56 +0000

    Working playground

--- Example/MyPlayground.playground/Contents.swift
+++ Example/MyPlayground.playground/Contents.swift
@@ -4,9 +4,6 @@ import UIKit
 import PlaygroundSupport
 import Sourcing
 
-let str = "hello"
-
-
 struct Train {
     var id: String
     var title: String
@@ -14,45 +11,34 @@ struct Train {
 
 class TrainCell: UITableViewCell, ConfigurableCell {
     typealias DataSource = Train
-    var titleLabel: UILabel!
     
     func configure(with noteBook: DataSource) {
-        titleLabel.text = noteBook.title
+        self.textLabel?.text = noteBook.title
     }
-    
 }
 
-class MyViewController: UIViewController, UITableViewDataSource {
+class TableViewController: UITableViewController {
     typealias DataProvider = ArrayDataProvider<Train>
     typealias CellConfig = CellConfiguration<TrainCell>
     let trainCellConfiguration = CellConfig(cellIdentifier: "TrainCell")
     var dataProvider: DataProvider!
     var dataSource: TableViewDataSource<DataProvider, CellConfig>!
     var trains: Array<Train> = []
-    var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addTableView()
+       tableView.register(TrainCell.self, forCellReuseIdentifier: "TrainCell")
         for i in 0..<6 {
-            trains.append(Train(id: "ID\(i)", title: "TEST \(i)"))
+            trains.append(Train(id: "ID\(i)", title: "Train \(i)"))
         }
         dataProvider = ArrayDataProvider(rows: trains, dataProviderDidUpdate: { [weak self] updates in
             self?.dataSource.processUpdates(updates)
         })
         dataSource = TableViewDataSource(tableView: tableView, dataProvider: dataProvider, cellDequable: trainCellConfiguration)
     }
-    
-    private func addTableView() {
-        view.frame = CGRect(x: 0, y: 0, width: 320, height: 480)
-        tableView = UITableView(frame: view.frame)
-        tableView.dataSource = self
-        tableView.register(TrainCell.self, forCellReuseIdentifier: "TrainCell")
-        view.addSubview(tableView)
-    }
-    
 }
-var ctrl = MyViewController()
-PlaygroundPage.current.liveView = ctrl.view
 
-
+var tableViewController = TableViewController()
+tableViewController.title = "Trains"
+let navigationController = UINavigationController(rootViewController: tableViewController)
+PlaygroundPage.current.liveView = navigationController.view
